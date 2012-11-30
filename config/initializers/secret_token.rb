014d10c1d3276f7a62ad505087d33e54afed0621
Ismar Dantas <ismardantasbs@hotmail.com>
2012-11-30 19:37:21 -0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitapp::Application.config.secret_token = 'eda49cdefd64f42fa8199edb5b1746edf5aa230eb4a401c3eaf97e748b99e3dd7d9cfe193e0157faa3597b31f806b05b540f4cdaf403868b92ea4d8ce8772aae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Twitapp::Application.config.session_store :cookie_store, key: '_twitapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Twitapp::Application.config.session_store :active_record_store
